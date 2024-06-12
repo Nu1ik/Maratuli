d167a9926962de3ae048ab6615237890dde169b7
Nu1ik <nurmuhanbet2006@icloud.com>
2024-06-12 12:12:26 +0500

    5

--- main.go
+++ main.go
@@ -1,46 +1,29 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "fmt" // новый импорт
+    "log"
+    "net/http"
+    "strconv" // новый импорт
 )
 
-// Обработчик главной страницы.
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Привет из Snippetbox"))
-}
+...
 
 // Обработчик для отображения содержимого заметки.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Отображение заметки..."))
+    // Извлекаем значение параметра id из URL и попытаемся 
+    // конвертировать строку в integer используя функцию strconv.Atoi(). Если его нельзя
+    // конвертировать в integer, или значение меньше 1, возвращаем ответ
+    // 404 - страница не найдена!
+    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    if err != nil || id < 1 {
+        http.NotFound(w, r)
+        return
+    }
+
+    // Используем функцию fmt.Fprintf() для вставки значения из id в строку ответа
+    // и записываем его в http.ResponseWriter.
+    fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
 }
 
-// Обработчик для создания новой заметки.
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	// Используем r.Method для проверки, использует ли запрос метод POST или нет. Обратите внимание,
-	// что http.MethodPost является строкой и содержит текст "POST".
-	if r.Method != http.MethodPost {
-		// Если это не так, то вызывается метод w.WriteHeader() для возвращения статус-кода 405
-		// и вызывается метод w.Write() для возвращения тела-ответа с текстом "Метод запрещен".
-		// Затем мы завершаем работу функции вызвав "return", чтобы
-		// последующий код не выполнялся.
-		w.WriteHeader(405)
-		w.Write([]byte("GET-Метод запрещен!"))
-		return
-	}
-
-	w.Write([]byte("Создание новой заметки..."))
-}
-
-func main() {
-	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
-	// маршрутизаторе servemux
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
-}
+...
